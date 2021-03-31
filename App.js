792ec0c39a38fe8260dbad9b4aca2645223abbfa
qod3r <primculov@protonmail.ch>
2021-03-31 18:34:07 +0700

    firebase fetch working

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import apiKeys from './config/keys';
 
 export default function App() {
+  const [state, setState] = useState ({
+    gChoice: false,
+    wChoice: false,
+    lessons: [],
+    selectedDay: 1
+  })
+
+  const fetchData = async () => {
+    const db = firebase.firestore();
+    const data = await db.collection('lessons').get();
+    setState({ lessons: data.docs.map((doc) => doc.data()) });
+  };
+
+  if (!firebase.apps.length) {
+    console.log("Firebase connected");
+    firebase.initializeApp(apiKeys.firebaseConfig);
+    fetchData();
+  }
+  console.log(state.lessons);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
