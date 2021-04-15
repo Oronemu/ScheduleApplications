a199956d845879884a072d8e58ae254795904ce0
Ivan Rovkov <Oronemu@mail.ru>
2021-04-15 20:59:51 +0700

    WebView stub on "schedule" screen created

--- Shedule/Shedule/Tabbar/schedule/ScheduleViewController.swift
+++ Shedule/Shedule/Tabbar/schedule/ScheduleViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
+import WebKit
 
-class ScheduleViewController: UIViewController {
+class ScheduleViewController: UIViewController, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let url = URL(string: "https://kemsu-schedule.web.app")!
+        webView.load(URLRequest(url: url))
     }
 }
