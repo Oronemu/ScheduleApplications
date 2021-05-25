8b1c15c19d82b7cb0361fa14032ff8dfe19b60f4
ass_kicker54 <ass_kicker54@pop-os.localdomain>
2021-05-25 13:46:49 +0700

    firebase

--- app/src/main/java/com/itdungeon/scheduleapp/MainActivity.kt
+++ app/src/main/java/com/itdungeon/scheduleapp/MainActivity.kt
@@ -3,27 +3,64 @@ package com.itdungeon.scheduleapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        //requestWindowFeature(Window.FEATURE_NO_TITLE)
-        //setContentView(R.layout.fragment_first)
 
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
-        val navController = findNavController(R.id.fragment)
-        val appBarConfiguration =  AppBarConfiguration(setOf(R.id.firstFragment, R.id.secondFragment))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val firstLessonStartTime:TextView = findViewById(R.id.textView1)
+        val firstLessonName:TextView = findViewById(R.id.textView2)
+        val firstLessonEndTime:TextView = findViewById(R.id.textView3)
+        val firstLessonType:TextView = findViewById(R.id.textView4)
+        val firstLessonCabinet:TextView = findViewById(R.id.textView5)
+        val firstLessonTeacher:TextView = findViewById(R.id.textView6)
 
-        bottomNavigationView.setupWithNavController(navController)
+        val secondLessonStartTime:TextView = findViewById(R.id.textView7)
+        val secondLessonName:TextView = findViewById(R.id.textView8)
+        val secondLessonEndTime:TextView = findViewById(R.id.textView9)
+        val secondLessonType:TextView = findViewById(R.id.textView10)
+        val secondLessonCabinet:TextView = findViewById(R.id.textView11)
+        val secondLessonTeacher:TextView = findViewById(R.id.textView12)
 
+        val thirdLessonStartTime:TextView = findViewById(R.id.textView13)
+        val thirdLessonName:TextView = findViewById(R.id.textView14)
+        val thirdLessonEndTime:TextView = findViewById(R.id.textView15)
+        val thirdLessonType:TextView = findViewById(R.id.textView16)
+        val thirdLessonCabinet:TextView = findViewById(R.id.textView17)
+        val thirdLessonTeacher:TextView = findViewById(R.id.textView18)
+
+        val fourthLessonStartTime:TextView = findViewById(R.id.textView19)
+        val fourthLessonName:TextView = findViewById(R.id.textView20)
+        val fourthLessonEndTime:TextView = findViewById(R.id.textView21)
+        val fourthLessonType:TextView = findViewById(R.id.textView22)
+        val fourthLessonCabinet:TextView = findViewById(R.id.textView23)
+        val fourthLessonTeacher:TextView = findViewById(R.id.textView24)
+
+        val mondayButton:Button = findViewById(R.id.button13)
+        val thuButton:Button = findViewById(R.id.button14)
+
+
+        mondayButton.setOnClickListener{
+            firstLessonStartTime.text = "9:45"
+            firstLessonName.text = "ОБЖ"
+            firstLessonEndTime.text = "11:45"
+            firstLessonType.text = "Практика"
+            firstLessonCabinet.text = "Ауд. 5119"
+            firstLessonTeacher.text = "Ефимов Д.А."
+        }
+
+        thuButton.setOnClickListener{
+            firstLessonStartTime.text = "15.30"
+        }
     }
+
 }
+
